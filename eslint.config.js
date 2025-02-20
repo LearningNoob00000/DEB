152ff5656d735eb1fe84ad7612876b288e94f1bb
SpongeBUG <132822564+SpongeBUG@users.noreply.github.com>
2025-02-20 21:36:42 +0800

    fix: improve versioning

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+// eslint.config.js
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      'coverage/**',
+      '*.js',
+      '!.eslintrc.js'
+    ]
+  },
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'prettier': prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn'
+    }
+  }
+];
